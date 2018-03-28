ce120d192e1aff730fda37ca435048492579460f
slavich <mo.slavich@mail.ru>
2018-03-29 03:35:58 +0700

    server thread

--- Server/SocketServer.cs
+++ Server/SocketServer.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace Server
 {
@@ -9,11 +10,11 @@ namespace Server
     {
         // Incoming data from the client.  
         public static string data = null;
+        static Socket handler;
 
         public static void StartListening()
         {
-            // Data buffer for incoming data.  
-            byte[] bytes = new Byte[1024];
+            
 
             // Establish the local endpoint for the socket.  
             // Dns.GetHostName returns the name of the   
@@ -36,34 +37,12 @@ namespace Server
                 // Start listening for connections.  
                 Console.WriteLine("Waiting for a connection...");
                 // Program is suspended while waiting for an incoming connection.  
-                Socket handler = listener.Accept();
+                handler = listener.Accept();
 
-                while (true)
-                {
-                    data = null;
+                Thread thread = new Thread(DoServer);
+                thread.Start();
 
-                    // An incoming connection needs to be processed.  
-                    bytes = new byte[1024];
-                    int bytesRec = handler.Receive(bytes);
-                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                    
-                    // Show the data on the console.  
-                    Console.WriteLine("Text received : {0}", data);
-
-                    CommandSplitter commandSplitter = new CommandSplitter(data);
-                    string[] separetedData = commandSplitter.ParseData();
-
-                    CommandHandler commandHandler = new CommandHandler(separetedData);
-                    string answer = commandHandler.HandleCommand();
-
-                    // Echo the data back to the client.  
-                    byte[] msg = Encoding.ASCII.GetBytes(answer);
-
-                    handler.Send(msg);
-                    Console.WriteLine("send", answer);
-                    //handler.Shutdown(SocketShutdown.Both);
-                    //handler.Close();
-                }
+                return;
 
             }
             catch (Exception e)
@@ -74,6 +53,39 @@ namespace Server
             Console.WriteLine("\nPress ENTER to continue...");
             Console.Read();
 
+        }
+
+        private static void DoServer()
+        {
+            // Data buffer for incoming data.  
+            byte[] bytes = new Byte[1024];
+
+            while (true)
+            {
+                data = null;
+
+                // An incoming connection needs to be processed.  
+                bytes = new byte[1024];
+                int bytesRec = handler.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                // Show the data on the console.  
+                Console.WriteLine("Text received : {0}", data);
+
+                CommandSplitter commandSplitter = new CommandSplitter(data);
+                string[] separetedData = commandSplitter.ParseData();
+
+                CommandHandler commandHandler = new CommandHandler(separetedData);
+                string answer = commandHandler.HandleCommand();
+
+                // Echo the data back to the client.  
+                byte[] msg = Encoding.ASCII.GetBytes(answer);
+
+                handler.Send(msg);
+                Console.WriteLine("send", answer);
+                //handler.Shutdown(SocketShutdown.Both);
+                //handler.Close();
+            }
         }
     }
 }
